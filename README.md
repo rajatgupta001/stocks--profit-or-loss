d996de6b0080b03b640ddaaaac03d8e5c5a9159a
Rajat Gupta <dguptarajat@gmail.com>
2021-09-24 09:55:35 +0530

    basic app completed

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # stocks: profit or loss
  
+
+ else if: works in JS.
+
+ for external CSS:
+ <link rel="stylesheet" href="mystyle.css">
+
+placeholder in input tag: 
+  <input type="number" placeholder="Initial Stock Price" id="initial-price">
---
+++ app.js
@@ -0,0 +1,28 @@
+const initialPrice = document.querySelector("#initial-price");
+const numberShare = document.querySelector("#number-of-shares");
+const currentPrice = document.querySelector("#current-price");
+const btnResult = document.querySelector("#btn-result");
+const result = document.querySelector("#result")
+
+function profitOrLoss(){
+    const initial = initialPrice.value;
+    const current = currentPrice.value;
+
+    if(initial > current){
+        const loss = (initial-current)*numberShare.value;
+        const lossPercent = (initial - current) * 100/initial;
+        result.innerText = "Absolute loss is " + loss + " and percentage loss is " + lossPercent; 
+        console.log("ladcwm");
+    }
+    else if(initial < current){
+        const profit = (current-initial)*numberShare.value;
+        const profitPercent = (current-initial)*100/initial;
+        result.innerText = "Absolute profit is " + profit + " and percentage profit is " + profitPercent;
+    }
+    else{
+        result.innerText = "no pain, no gain and no gain, no pain";
+    }
+    }
+
+btnResult.addEventListener("click", profitOrLoss);
+
